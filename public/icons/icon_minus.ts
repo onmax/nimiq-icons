b038aea0043eb6d25856ae66c83b7fd498f6f131
onmax <onmax@users.noreply.github.com>
2023-01-25 22:44:38 +0000

    Add new icon: Minus

---
+++ public/icons/icon_minus.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "minus",
+  keywords: ["minus", "subtraction", "decrease", "reduce", "lower", "diminish", "decline", "subtract", "difference", "negative", "opposite", "remove", "delete"],
+  svg: '<svg width="24" height="24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.133" d="M19 12H5"/></svg>'
+} as Icon;
