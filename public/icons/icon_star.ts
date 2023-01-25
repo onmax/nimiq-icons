0b8f60ab5b50a398787e04293bee8a68c6cb7f3c
onmax <onmax@users.noreply.github.com>
2023-01-25 22:49:39 +0000

    Add new icon: Star

---
+++ public/icons/icon_star.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "star",
+  keywords: ["star", "rating", "favorite", "mark", "label", "tag", "symbol", "icon", "illustration", "mark as favorite", "rating system", "give a star", "five-star", "star rating", "star icon", "star symbol", "star illustration", "favourite"],
+  svg: '<svg width="14" height="15" fill="none"><path fill="url(#a)" d="M7.345 12.074a.25.25 0 0 1 .245 0l3.113 1.752a1.276 1.276 0 0 0 1.5-.166 1.256 1.256 0 0 0 .316-1.45l-1.22-2.812a.25.25 0 0 1 .064-.286l2.212-1.971a1.262 1.262 0 0 0 .3-1.378 1.232 1.232 0 0 0-1.17-.765h-2.5a.25.25 0 0 1-.23-.151L8.633 1.73l-.012-.028a1.294 1.294 0 0 0-2.318.028L4.957 4.846a.25.25 0 0 1-.23.15h-2.5a1.228 1.228 0 0 0-1.166.76 1.268 1.268 0 0 0 .319 1.4l2.194 1.956a.251.251 0 0 1 .063.286l-1.223 2.815a1.253 1.253 0 0 0 .314 1.45 1.277 1.277 0 0 0 1.506.167l3.11-1.756Z"/><defs><radialGradient id="a" cx="0" cy="0" r="1" gradientTransform="matrix(-12.9928 0 0 -13.0037 13.964 14.002)" gradientUnits="userSpaceOnUse"><stop stop-color="#EC991C"/><stop offset="1" stop-color="#E9B213"/></radialGradient></defs></svg>'
+} as Icon;
