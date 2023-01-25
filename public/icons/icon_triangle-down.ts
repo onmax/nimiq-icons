6bdb80b9a066c0e78f5fc0eac7606a589b937296
onmax <onmax@users.noreply.github.com>
2023-01-25 22:06:16 +0000

    Add new icon: Triangle down

--- public/icons/icon_triangle-down.ts
+++ public/icons/icon_triangle-down.ts
@@ -1,5 +1,5 @@
 export default {
   name: "triangle down",
   keywords: ["triangle", "equilateral", "filled", "pointing", "down", "shape", "symbol", "geometric", "angle", "form", "graphic", "design", "simple", "basic", "icon", "illustration", "math", "three", "corner", "vertex", "regular", "solid", "flat", "base", "point", "angle", "geometric", "angle"],
-  svg: "<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 24"><path fill="currentColor" d="M13.557 8.5a.8.8 0 0 1 .678 1.224l-3.557 5.69a.8.8 0 0 1-1.356 0l-3.557-5.69A.8.8 0 0 1 6.443 8.5h7.114Z"/></svg>"
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 24"><path fill="currentColor" d="M13.557 8.5a.8.8 0 0 1 .678 1.224l-3.557 5.69a.8.8 0 0 1-1.356 0l-3.557-5.69A.8.8 0 0 1 6.443 8.5h7.114Z"/></svg>'
 } as Icon;
