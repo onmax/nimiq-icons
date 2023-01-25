9a9b8c0ca50adcb216712d5be0468d44ddad312a
onmax <onmax@users.noreply.github.com>
2023-01-25 22:41:13 +0000

    Add new icon: List items

---
+++ public/icons/icon_list-items.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "list items",
+  keywords: ["list", "items", "bullet", "points", "numbered", "ordered", "unordered", "collection", "array", "set", "sequence", "enumeration", "register", "catalog", "inventory", "array", "item", "element", "entry", "point", "line", "record"],
+  svg: '<svg fill="none" viewBox="0 0 16 18"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 2.998h9"/><path fill="currentColor" d="M1.5 16.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM1.5 10.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM1.5 4.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 9h9M6 14.998h9"/></svg>'
+} as Icon;
