14915d1d3d03ff8bb1a11f2544f04b723c0b937c
onmax <onmax@users.noreply.github.com>
2023-01-25 22:26:37 +0000

    Add new icon: Arrow top right

---
+++ public/icons/icon_arrow-top-right.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "arrow top right",
+  keywords: ["arrow", "link", "direction", "navigation", "pointing", "move", "chain", "connection", "hyperlink", "reference", "url", "symbol", "icon", "illustration"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 11"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3.438 3.258H7.68V7.5m-.739-3.314L1.942 9.185"/></svg>'
+} as Icon;
