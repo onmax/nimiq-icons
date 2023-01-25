480b6b1693f5705ed81e1f5683785380d22b9471
maximo <maximo@nimiq.com>
2023-01-25 23:26:46 +0000

    fixed some icosn

--- public/icons/icon_arrow-down.ts
+++ public/icons/icon_arrow-down.ts
@@ -1,9 +1,9 @@
 export default {
     "name": "Arrow down",
-    "keywords": ["arrow", "down"],
+    "keywords": ["arrow", "down", "direction", "navigation", "pointing", "move", "next", "forward", "advance", "progress", "turn", "change", "shift", "lead", "indicate", "symbol", "sign", "graph", "chart", "diagram", "map", "flow", "guide", "indicator"],
     "svg": `
         <svg width="29" height="28" viewBox="0 0 29 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x=".5" width="28" height="28" rx="14" fill="#1F2348" fill-opacity=".1"/>
-            <path d="M14.496 20.165V8.5M19.272 15.39l-4.773 4.773-4.772-4.772" stroke="#1F2348" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <rect x=".5" width="28" height="28" rx="14" fill="currentColor" fill-opacity=".1"/>
+            <path d="M14.496 20.165V8.5M19.272 15.39l-4.773 4.773-4.772-4.772" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>`
 } as Icon;
