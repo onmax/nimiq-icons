9df20e38a4e0cd709eb4758b352ffd888ce66a9e
onmax <onmax@users.noreply.github.com>
2023-01-25 22:31:13 +0000

    Add new icon: check

---
+++ public/icons/icon_check.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "check",
+  keywords: ["check", "mark", "tick", "validate", "verify", "approve", "confirm", "accept", "agree", "correct", "right", "symbol"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" aria-hidden="true" viewBox="0 0 32 32"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m20.158 13-5.555 7-3.353-2.882"/></svg>'
+} as Icon;
