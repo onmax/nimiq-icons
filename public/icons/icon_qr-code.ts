5ab3a2f6929e3a878a620dac83bf9779d090bd22
Max <onmax@users.noreply.github.com>
2023-02-12 23:57:25 +0100

    New icons (#123)
    * Add new icon: swap
    
    * Add new icon: qr code
    
    * Add new icon: whatsapp
    
    * Add new icon: telegram
    
    * Add new icon: clipboard
    
    * Add new icon: mail
    
    * Add new icon: Cryptopayment.link

---
+++ public/icons/icon_qr-code.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "qr code",
+  keywords: ["qrcode","2dbarcode","quickresponsecode","mobilebarcode","scanning","datamatrix","barcodescanner","qrscanner","qrreader","smartphonescanning","qrcodegenerator","qrcodereader","qrcodescanner","qrcodetechnology","mobilescanning","digitalcode","mobiletechnology","digitalscanning","smartphonebarcode","smartphonetechnology","barcodetechnology","mobileqrcode","qrcoderecognition","qrcodedecoding","qrcodedetection","mobileqrcodescanning"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"><g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" opacity=".8"><path fill="currentColor" d="M5 1H1v4h4V1Zm0 13.999H1v4h4v-4ZM18.999 1h-4v4h4V1Z"/><path d="M1 11h8v1.999M13 13v6h6v-6h-2m-8 4v2M9 1v6h2m3.999 2h4"/></g></svg>'
+} as Icon;
