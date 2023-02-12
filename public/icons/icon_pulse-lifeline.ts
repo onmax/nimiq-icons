4be0d28768d97b455a49392528df30c22265364e
onmax <onmax@users.noreply.github.com>
2023-02-12 17:42:41 +0000

    Add new icon: Pulse lifeline

--- public/icons/icon_pulse-lifeline.ts
+++ public/icons/icon_pulse-lifeline.ts
@@ -1,5 +1,6 @@
 export default {
   name: "pulse lifeline",
-  keywords: ,
+  keywords: 
+[],
   svg: '<svg xmlns="http://www.w3.org/2000/svg" width="41" height="27" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 15.277h7l5.273-13.184a1.748 1.748 0 0 1 3.267.068l7.681 21.758a1.75 1.75 0 0 0 3.169.287l5.11-8.93h7" opacity=".4"/></svg>'
 } as Icon;
