07fa5dd6384b8482ad0e417249af5fddec545cd6
onmax <onmax@users.noreply.github.com>
2023-02-12 22:01:01 +0000

    Add new icon: mail

---
+++ public/icons/icon_mail.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "mail",
+  keywords: letlist=["picture","image","icon","visual","symbol","design","illustration","artwork","artistic","art","graphic","vector","artistic","style","creative","illustrative","artistic","representation","illustrate","visualize","depict","drawing","sketch","painting","coloring","shading"];letresult=JSON.stringify(list);console.log(result);,
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" fill="none"><path fill="currentColor" fill-opacity=".8" d="M10.068 7.995c.092.091.183.091.275 0l8.778-5.852c.183-.092.274-.274.183-.457a1.868 1.868 0 0 0-1.83-1.463H2.388C1.473.223.741.863.56 1.686c0 .183.091.365.274.548l9.235 5.761Z"/><path fill="currentColor" fill-opacity=".8" d="M11.074 10.096a1.512 1.512 0 0 1-1.737 0L1.29 4.976c-.274-.092-.549 0-.64.182-.091.092-.091.183-.091.275v8.96c0 1.007.823 1.83 1.828 1.83h15.088a1.834 1.834 0 0 0 1.829-1.83v-8.96c0-.275-.183-.457-.457-.457-.092 0-.183 0-.275.091l-7.498 5.03Z"/></svg>'
+} as Icon;
