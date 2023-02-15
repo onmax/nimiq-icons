a4fc291dc2871284846601be79c81537f1531a16
Max <onmax@users.noreply.github.com>
2023-02-15 12:30:13 +0100

    New icons (#125)
    * Add new icon: swap
    
    * Add new icon: qr code
    
    * Add new icon: whatsapp
    
    * Add new icon: telegram
    
    * Add new icon: clipboard
    
    * Add new icon: mail
    
    * Add new icon: Cryptopayment.link
    
    * Add new icon: Globe

---
+++ public/icons/icon_globe.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "globe",
+  keywords: ["globe","world","planet","earth","sphere","global","continents","map","geography","geology","geodesy","international","countries","atlas","rotation","meridian","longitude","latitude","astronomy","cosmology","ecology","environment","climate","timezones","hemisphere","solarsystem"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 30c7.732 0 14-6.268 14-14S23.732 2 16 2 2 8.268 2 16s6.268 14 14 14ZM2.798 11.333h26.404m-25.933 10.5h25.46"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.02 29.87a26.863 26.863 0 0 1 0-27.723m3.96 27.723a26.864 26.864 0 0 0 0-27.723"/></svg>'
+} as Icon;
