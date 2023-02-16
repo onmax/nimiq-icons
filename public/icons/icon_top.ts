4e6149da7a02256050c3a6125ee77dd6f54e15d5
onmax <onmax@users.noreply.github.com>
2023-02-16 10:42:22 +0000

    Add new icon: Top

---
+++ public/icons/icon_top.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "top",
+  keywords: ["top","highest","highestranking","topranking","most","best","topmost","highestrated","toprated","peak","superior","supreme","eloquent","acme","acmeofperfection","acmeofexcellence","pinnacle","apex","zenith","crest","summit","head","headoftheclass","headofthepack","creamofthecrop","first-rate","first-class","outstanding"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 12"><g stroke="currentColor" stroke-linecap="round" stroke-width="1.5" clip-path="url(#a)"><path stroke-linejoin="round" d="M.75 7.5 4 4.25 7.25 7.5M4 5.75v5"/><path d="M7.25 1.25H.75"/></g><defs><clipPath id="a"><path fill="#fff" d="M8 12H0V0h8z"/></clipPath></defs></svg>'
+} satisfies Icon;
