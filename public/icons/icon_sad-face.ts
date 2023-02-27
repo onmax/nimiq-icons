a5addff8f948dc1328719a2245957f0818191082
onmax <onmax@users.noreply.github.com>
2023-02-27 17:56:28 +0000

    Add new icon: Sad face

---
+++ public/icons/icon_sad-face.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "sad face",
+  keywords: ["sad","unhappy","depressed","gloomy","downcast","despondent","forlorn","dejected","melancholy","discouraged","despairing","desolate","blue","mournful","glum","dismal","doleful","heartbroken","disheartened","downhearted","low","oppressed","anguished","pessimistic","wretched","lonely"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="102" height="102"><circle cx="51" cy="51" r="48" fill="none" stroke="currentColor" stroke-width="6"/><circle cx="35.148" cy="40.663" r="4.824" fill="currentColor"/><circle cx="66.851" cy="40.662" r="4.824" fill="currentColor"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6" d="M39.973 70.987S44.797 68.23 51 68.23c6.203 0 11.027 2.757 11.027 2.757"/></svg>'
+} satisfies Icon;
