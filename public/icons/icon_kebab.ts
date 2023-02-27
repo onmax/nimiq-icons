6c01dede2b3be39bacb33c80c3e39fc395960ff6
onmax <onmax@users.noreply.github.com>
2023-02-27 17:59:47 +0000

    Add new icon: Kebab

---
+++ public/icons/icon_kebab.ts
@@ -0,0 +1,5 @@
+export default {
+  name: "kebab",
+  keywords: ["kebab","shawarma","doner","meat","grilled","turkish","middle-eastern","spicy","marinated","flatbread","vegetables","sauce","dip","wrap","sandwich","gyro","döner","kebap","tortilla","pita","skewer","lamb","beef","chicken","vegan","vegetarian"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="7" height="30"><g fill="currentColor"><circle cx="3.5" cy="3" r="3"/><circle cx="3.5" cy="15" r="3"/><circle cx="3.5" cy="27" r="3"/></g></svg>'
+} satisfies Icon;
