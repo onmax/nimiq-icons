06b485c689f209a03dfc09101eaaacf0e5d93b34
onmax <onmax@users.noreply.github.com>
2023-02-28 11:17:45 +0000

    Add new icon: Cashlink Large

---
+++ public/icons/icon_cashlink-large.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "cashlink large",
+  keywords: ["cashlink","payment","sendmoney","onlinetransfer","moneytransfer","financialtransaction","securepayment","securetransfer","payonline","paysecurely","payquickly","onlinepayment","onlinemoneytransfer","moneytransferservice","financialservices","financialserviceprovider","paymentprovider","safemoneytransfer","securemoneytransfer","fastpayment","instantpayment","electronicpayment","digitalpayment","digitalmoneytransfer","onlinemoneytransferservice","paymentplatform","digitalpaymentplatform"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="64" height="64"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5"><path d="M40.25 23.25v-.5a6.5 6.5 0 0 0-6.5-6.5h-3.5a6.5 6.5 0 0 0-6.5 6.5v6.5a6.5 6.5 0 0 0 6.5 6.5h2"/><path d="M23.75 40.75v.5a6.5 6.5 0 0 0 6.5 6.5h3.5a6.5 6.5 0 0 0 6.5-6.5v-6.5a6.5 6.5 0 0 0-6.5-6.5h-2M32 11.25v4M32 48.75v4"/></g></svg>',
+  category: "Large",
+} satisfies Icon;
