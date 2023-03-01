32d20a8e67fa522f06f496c4f7cac79120bba82d
onmax <onmax@users.noreply.github.com>
2023-03-01 14:03:19 +0000

    Add new icon: Digital gold

---
+++ public/icons/icon_digital-gold.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "digital gold",
+  keywords: ["digitalgold","cryptocurrency","bitcoin","altcoin","blockchain","cryptoassets","cryptotrading","investing","mining","cryptocurrencyexchange","cryptowallet","digitalasset","cryptomarket","cryptoportfolio","decentralizedfinance","stablecoin","token","cryptopayment","cryptocurrency","cryptosecurity","cryptobanking","cryptotradingplatform","cryptoinvesting","cryptomining","cryptoexchange","cryptostorage"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="136" height="136" fill="none"><path fill="currentColor" d="M60 71.7a3.6 3.6 0 0 0-3.6-2.8H37.1a3.6 3.6 0 0 0-3.5 2.8L28 93.2a3.6 3.6 0 0 0 3.5 4.4H62a3.6 3.6 0 0 0 3.5-4.4l-5.6-21.5Zm-5-34-5.5 21.5a3.6 3.6 0 0 0 3.5 4.4h30.5a3.6 3.6 0 0 0 3.5-4.4l-5.6-21.5a3.6 3.6 0 0 0-3.5-2.8H58.6a3.6 3.6 0 0 0-3.5 2.8Zm35.6 31.2h7.2v7.2h-7.2zm7.2 7.2h7.2v7.2h-7.2zm-7.2 7.2h7.2v7.2h-7.2zm14.4 0h7.2v7.2H105zm-7.2 7.2h7.2v7.2h-7.2z"/><path fill="currentColor" fill-rule="evenodd" d="M92.7 97.6h-2V69H80a3.6 3.6 0 0 0-3.5 2.8L71 93.2a3.6 3.6 0 0 0 3.5 4.4h18.2Z" clip-rule="evenodd"/></svg>',
+  category: "Large",
+} satisfies Icon;
