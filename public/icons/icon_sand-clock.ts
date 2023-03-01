809fc2b9966c76ab2e0f4832a17e6a27871c20c5
onmax <onmax@users.noreply.github.com>
2023-03-01 14:26:18 +0000

    Add new icon: Sand clock

---
+++ public/icons/icon_sand-clock.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "sand clock",
+  keywords: ["sandclock","hourglass","time","measurement","measure","countdown","timer","glass","sand","flow","minute","second","duration","endless","infinity","endurance","endure","wait","waiting","limit","limited","limitless","temporal","temporality","temporarily","eternity","forever"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path fill="currentColor" d="M16 17.83V15.8a4.15 4.15 0 0 0-2.51-3.8A4.15 4.15 0 0 0 16 8.2V6a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2.21A4.15 4.15 0 0 0 10.51 12 4.15 4.15 0 0 0 8 15.8V18a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-.17Zm-6.18-8.8a.58.58 0 0 1 .51-.31h3.34a.56.56 0 0 1 .5.32.6.6 0 0 1-.04.6A2.64 2.64 0 0 1 12 10.76a2.58 2.58 0 0 1-2.13-1.12.6.6 0 0 1-.05-.6Zm-.15 6.84 1.74-1.72a.87.87 0 0 1 1.18 0l1.74 1.72a.8.8 0 0 1 .15.93.85.85 0 0 1-.74.45h-3.48a.82.82 0 0 1-.82-.94.8.8 0 0 1 .23-.44Z"/></svg>',
+  category: "Small",
+} satisfies Icon;
