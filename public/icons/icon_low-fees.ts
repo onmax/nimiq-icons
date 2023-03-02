11faa9d7d3f6321fed2b9bb90dfe28a3b4808b55
onmax <onmax@users.noreply.github.com>
2023-03-02 09:32:19 +0000

    Add new icon: Low fees

---
+++ public/icons/icon_low-fees.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "low fees",
+  keywords: ["lowfees","affordable","cheap","discounted","cost-effective","budget-friendly","low-cost","economical","wallet-friendly","bargain","reasonable","low-priced","value","low-priced","discount","cut-price","cut-rate","reasonable","budget","inexpensive","cheapest","inexpensively","economize","economizing","affordably","thrifty"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none"><g fill="currentColor" stroke="currentColor" stroke-linejoin="round" stroke-width="3" opacity=".4"><rect width="15.25" height="15.25" x="1.5" y="1.5" rx="2"/><rect width="15.25" height="15.25" x="1.5" y="16.75" rx="2"/><rect width="15.25" height="15.25" x="16.75" y="16.75" rx="2"/><rect width="15.25" height="15.25" x="1.5" y="32" rx="2"/><rect width="15.25" height="15.25" x="16.75" y="32" rx="2"/><rect width="15.25" height="15.25" x="32" y="32" rx="2"/><rect width="15.25" height="15.25" x="1.5" y="47.25" rx="2"/><rect width="15.25" height="15.25" x="16.75" y="47.25" rx="2"/><rect width="15.25" height="15.25" x="32" y="47.25" rx="2"/></g><rect width="15.25" height="15.25" x="47.25" y="47.25" stroke="currentColor" stroke-linejoin="round" stroke-width="3" rx="2"/></svg>',
+  category: "Large",
+} satisfies Icon;
