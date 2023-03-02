e25d9bcc588ef8e5d49b1337d45793919426f98c
onmax <onmax@users.noreply.github.com>
2023-03-02 08:44:57 +0000

    Add new icon: Projection screen

---
+++ public/icons/icon_projection-screen.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "projection screen",
+  keywords: ["projectionscreen","screen","projectorscreen","projection","projector","projecting","display","displayscreen","projectedimage","projecteddisplay","projectedvideo","projectedmonitor","projectedtv","projectingdevice","projectordevice","projecteddevice","projectingmonitor","projectingtv","projectingvideo","projectingimage","projectedmonitor","projectedtv","projecteddevice","projectingdisplay","projectingtechnology","projectedtechnology"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M14.801 22.769v2.312M26.134 4.845H3.471v17.807h22.663V4.845ZM14.8 28.899a1.621 1.621 0 0 0 1.62-1.618 1.62 1.62 0 1 0-1.62 1.618Z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M1.852 22.655h25.901M3.471 18.608c1.831-1.97 7.327-7.69 7.327-7.69.381.805 2.357 4.56 2.357 4.56.49-.75 2.732-3.953 2.732-3.953l2.252 2.703s1.975-3.748 2.329-4.524m8.093-4.857H1.041V.8h27.52v4.047Z"/></svg>',
+  category: "Large",
+} satisfies Icon;
