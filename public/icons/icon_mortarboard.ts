fb7a6b9399c5262cc2f4a47f967f315ebea295b5
onmax <onmax@users.noreply.github.com>
2023-03-02 08:55:18 +0000

    Add new icon: mortarboard

---
+++ public/icons/icon_mortarboard.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "mortarboard",
+  keywords: ["mortarboard","graduationcap","academichat","ceremonialhat","squareacademiccap","capandgown","scholarscap,biretta,tamoshanter","gradhat","universityhat","ceremonialheadgear","capandtassel","capandhood","academicdress","commencementcap","universitycap","academicregalia","capandgownceremony","scholarlyheadgear","scholarlyhat","graduationregalia","scholarshat,scholarsheadgear","academicheadgear","academiccap","graduationheadgear"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 56 35"><path d="M4.5 6.507v14.347M29.072 10.78c.282.05.572.05.855 0l24.782-4.31a.625.625 0 0 0 0-1.23L29.927.93a2.455 2.455 0 0 0-.855 0L4.289 5.24a.625.625 0 0 0 0 1.23l24.783 4.31ZM4.5 20.855a27.31 27.31 0 0 0-3.705 11.25 1.165 1.165 0 0 0 .703 1.153c.147.064.306.097.467.097h5a1.25 1.25 0 0 0 1.25-1.346A27.388 27.388 0 0 0 4.5 20.855Z"/><path d="M14.5 8.247v18.108a47.443 47.443 0 0 0 30 0V8.247"/></svg>',
+  category: "Large",
+} satisfies Icon;
