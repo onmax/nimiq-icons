4ecc26e19f239e738650592cc9fb9cf1daae75cf
onmax <onmax@users.noreply.github.com>
2023-03-02 09:32:40 +0000

    Add new icon: No chargeback

---
+++ public/icons/icon_no-chargeback.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "no chargeback",
+  keywords: ["nochargeback","norefunds","noreversals","noliabilities","nofinancialrisk","financialsecurity","financialprotection","nomoneyloss","guaranteedpayment","guaranteedtransaction","safepayment","safetransaction","securepayment","securetransaction","risk-freepurchase","risk-freepayment","no-riskpurchase","no-riskpayment","no-risktransaction","no-hasslepurchase","no-hasslepayment","no-hassletransaction","zero-riskpurchase","zero-riskpayment","zero-risktransaction"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M60 4 4 60"/><path fill="currentColor" d="M44.131 13.498H31.174V5.84a3.333 3.333 0 0 0-5.777-2.273l-.013.014-21.51 24.414a5.166 5.166 0 0 0 0 7.015l8.78 9.965 31.477-31.477Z" opacity=".4"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M49 14.998h10v3.666l-.005 14.663V47.99H29.67v9.172a1.834 1.834 0 0 1-3.177 1.252l-9.76-11.078"/></svg>',
+  category: "Large",
+} satisfies Icon;
