ae6a20f4384679a57c2164fcb680df8ad8b19eb7
Max <onmax@users.noreply.github.com>
2023-03-02 10:51:12 +0100

    Update icon_seeding.ts

--- public/icons/icon_seeding.ts
+++ public/icons/icon_seeding.ts
@@ -1,6 +1,6 @@
 export default {
   name: "seeding",
   keywords: ["seeding","planting","germination","growth","cultivation","sowing","farming","harvesting","sprouting","fertilizing","blooming","soil","watering","crops","gardening","flowers","vegetables","fruits","trees","seeds","cultivate","nurture","cultivator","greenhouse","landscaping"],
-  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12.05 12.04v5.63m5.38-7.32c0 3.56-1.87 5.43-5.43 5.43 0-5.05 1.57-5.43 5.43-5.43ZM7.25 7.3c0 3.86 1.19 5.77 4.75 5.77 0-3.86-.3-5.77-4.75-5.77Z"/></svg>',
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12.05 12.04v5.63m5.38-7.32c0 3.56-1.87 5.43-5.43 5.43 0-5.05 1.57-5.43 5.43-5.43ZM7.25 7.3c0 3.86 1.19 5.77 4.75 5.77 0-3.86-.3-5.77-4.75-5.77Z"/></svg>',
   category: "Small",
 } satisfies Icon;
