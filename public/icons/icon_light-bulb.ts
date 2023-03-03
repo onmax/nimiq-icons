76d9ed0b752e179988ffeb267d09e3d98998621f
onmax <onmax@users.noreply.github.com>
2023-03-03 10:47:29 +0000

    Add new icon: Light bulb

---
+++ public/icons/icon_light-bulb.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "light bulb",
+  keywords: ["lightbulb","illumination","idea","innovation","creativity","inspiration","light","lamp","bulb","electricity","energy","illuminate","brightness","ideas","concept","invent","inspire","glow","electric","bright","luminous","radiance","spark","glowing","brilliance","enlightenment"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M32.01 3.01v7.56m-21.39 1.3 5.35 5.35m37.43-5.35-5.34 5.35M3.03 33.26h7.56m50.41 0h-7.56"/><path fill="currentColor" fill-rule="evenodd" d="M40 46.37a16 16 0 1 0-16 0v6.13a8 8 0 1 0 16 0v-6.13z" clip-rule="evenodd" opacity=".4"/></svg>',
+  category: "Large",
+} satisfies Icon;
