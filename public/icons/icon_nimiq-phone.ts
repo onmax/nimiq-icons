54b3eb6aad67439d429be7120058808ba966715a
Max <onmax@users.noreply.github.com>
2023-03-03 14:14:23 +0100

    New icons (#194)
    * Add new icon: swap
    
    * Add new icon: qr code
    
    * Add new icon: whatsapp
    
    * Add new icon: telegram
    
    * Add new icon: clipboard
    
    * Add new icon: mail
    
    * Add new icon: Cryptopayment.link
    
    * Add new icon: Globe
    
    * Add new icon: Top
    
    * Add new icon: lighting
    
    * Add new icon: Street cone
    
    * Add new icon: Cycle
    
    * Add new icon: Party
    
    * Add new icon: Text input
    
    * Add new icon: Double arrow
    
    * Add new icon: usdc
    
    * Add new icon: Sputnik
    
    * Add new icon: swap
    
    * Add new icon: upload
    
    * Add new icon: Bars
    
    * Add new icon: Gear
    
    * Add new icon: Sad face
    
    * Add new icon: Keys
    
    * Add new icon: Eye
    
    * Add new icon: Crossed eye
    
    * Add new icon: Payment pending
    
    * Add new icon: Transfer
    
    * Add new icon: Question mark
    
    * Add new icon: Plus circle
    
    * Add new icon: Kebab
    
    * Add new icon: Circle arrow
    
    * Add new icon: Locked lock
    
    * Add new icon: Cashlink
    
    * Add new icon: Cashlink small
    
    * Add new icon: Contact Book
    
    * Add new icon: Poker face
    
    * Add new icon: ledger
    
    * Add new icon: Unlocked lock
    
    * Add new icon: Cashlink Large
    
    * Improved UI and UX
    
    * Added category to workflow
    
    * Fixed icons
    
    * Fixed icons
    
    * Add new icon: bank
    
    * Add new icon: Digital gold
    
    * Add new icon: Sparkling swap
    
    * Added viewBox
    
    * Added viewBox
    
    * Add new icon: hand
    
    * Add new icon: Seeding
    
    * Add new icon: Resize
    
    * Add new icon: Sand clock
    
    * Add new icon: Weighing scale
    
    * Add new icon: Dollar
    
    * Add new icon: Centralised
    
    * Add new icon: Developer
    
    * Add new icon: Projection screen
    
    * Add new icon: lectern
    
    * Add new icon: Conversation bubbles
    
    * Add new icon: Group
    
    * Add new icon: Writer
    
    * Add new icon: Seeding large
    
    * Add new icon: Graduate
    
    * Add new icon: mortarboard
    
    * Add new icon: Handshake
    
    * Add new icon: Low fees
    
    * Add new icon: No chargeback
    
    * Add new icon: International
    
    * Add new icon: Speedmeter
    
    * Add new icon: Shield
    
    * Add new icon: Privacy
    
    * Add new icon: Target
    
    * Update icon_dollar.ts
    
    * Update icon_centralised.ts
    
    * Update icon_lectern.ts
    
    * Update icon_projection-screen.ts
    
    * Update icon_seeding.ts
    
    * Add new icon: Languages
    
    * Add new icon: Nano ledger
    
    * Add new icon: Light bulb
    
    * Add new icon: Nimiq phone
    
    * Add new icon: Smartphone
    
    * Add new icon: QR code large
    
    * Add new icon: Flame

---
+++ public/icons/icon_nimiq-phone.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "nimiq phone",
+  keywords: ["nimiq","phone","cellphone","smartphone","mobile","handheld","wireless","handset","communication","connectivity","4g","lte","data","talk","text","internet","mobiledevice","mobilenetwork","cellularnetwork","calling","messaging","apps","gps","camera","multimedia","touchscreen"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64"><path fill="currentColor" d="M19 52a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M36.5 42v15.3c0 3.07-2.13 5.2-5.2 5.2H6.5a4.98 4.98 0 0 1-5-4.98V6.48a4.98 4.98 0 0 1 5-4.98h20.83"/><path fill="currentColor" d="m61.3 17.58-7-12.16A2.87 2.87 0 0 0 51.87 4H37.78c-1 0-1.91.5-2.41 1.42l-7 12.16c-.5.84-.5 1.92 0 2.84l7 12.16c.5.84 1.41 1.42 2.41 1.42h14.09c1 0 1.91-.5 2.41-1.42l7-12.16c.59-.92.59-1.92 0-2.84z" opacity=".4"/><path stroke="currentColor" stroke-width="3" d="M36.5 46.5H2"/></svg>',
+  category: "Large",
+} satisfies Icon;
---
+++ public/icons/icon_qr-code-large.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "qr code large",
+  keywords: ["qrcode","2dbarcode","matrixbarcode","scanningcode","mobilescanning","smartphonescanning","instantaccess","instantinformation","fastaccess","datatransfer","datatransmission","datasharing","digitalcode","digitalinformation","digitaltransfer","encodedinformation","encodeddata","mobilecode","mobileinformation","mobiletransfer","smartphonecode","smartphoneinformation","smartphonetransfer","quickresponsecode","quickresponsedata"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64"><path fill="currentColor" d="M53 9H43a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V11a2 2 0 0 0-2-2zM21 9H11a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V11a2 2 0 0 0-2-2zm0 32H11a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V43a2 2 0 0 0-2-2z" opacity=".4"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M10.5 34H30v4m8 0v15h15V38h-5m-18 8v7m0-42.5V26h4m8 4h11M1.5 15.5v-10a4 4 0 0 1 4-4h10m33 0h10a4 4 0 0 1 4 4v10m0 33v10a4 4 0 0 1-4 4h-10m-33 0h-10a4 4 0 0 1-4-4v-10"/></svg>',
+  category: "Large",
+} satisfies Icon;
