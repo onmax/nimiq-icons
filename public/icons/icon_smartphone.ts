82004810c8b0a4773053f7992191917b87e4335c
onmax <onmax@users.noreply.github.com>
2023-03-03 10:50:31 +0000

    Add new icon: Smartphone

---
+++ public/icons/icon_smartphone.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "smartphone",
+  keywords: ["smartphone","mobilephone","cellphone","cellularphone","iphone","android","phone","mobiledevice","telephone","mobile","handhelddevice","communicationdevice","mobiletechnology","cellulartechnology","touchscreen","phonescreen","phonecall","texting","textmessage","bluetooth","wireless","apps","applications","camera","internet","connectivity"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 38 64"><path fill="currentColor" d="M19.5 51a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"/><path stroke="currentColor" stroke-width="3" d="M35.5 45.5H1"/><rect width="35" height="61" x="1.5" y="1.5" stroke="currentColor" stroke-width="3" rx="4.5"/></svg>',
+  category: "Large",
+} satisfies Icon;
