1197a485b7656a26234a3ef1af7cdac5814be4c0
onmax <onmax@users.noreply.github.com>
2023-03-03 10:46:56 +0000

    Add new icon: Nano ledger

---
+++ public/icons/icon_nano-ledger.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "nano ledger",
+  keywords: ["nanoledger","cryptocurrencyhardwarewallet","securestorage","digitalassetprotection","cryptocurrencysafety","blockchainsecurity","coldstorage","bitcoinwallet","ethereumwallet","altcoinwallet","digitalassetwallet","cryptocurrencywallet","hardwarewallet","offlinewallet","cryptohardwarewallet","securecryptowallet","coldstoragewallet","cryptosecurity","bitcoinsecurity","ethereumsecurity","altcoinsecurity","blockchainwallet","digitalassetsecurity","digitalcurrencywallet","digitalcurrencysecurity","digitalassetmanagement"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64"><path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" d="M14 54a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"/><path stroke="currentColor" stroke-width="3" d="M1.5 6.86C1.5 3.9 3.9 1.5 6.86 1.5h14.28c2.96 0 5.36 2.4 5.36 5.36V50a12.5 12.5 0 1 1-25 0V6.86z"/><path fill="currentColor" fill-rule="evenodd" d="m22.78 58.9 36.87-27.46a4 4 0 0 0 .82-5.6L50.3 12.21a4 4 0 0 0-5.6-.82L26.5 24.95V50c0 3.48-1.42 6.63-3.72 8.9z" clip-rule="evenodd" opacity=".4"/></svg>',
+  category: "Large",
+} satisfies Icon;
