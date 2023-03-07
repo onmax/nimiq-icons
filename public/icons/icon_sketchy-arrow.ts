3322a6fe70837a1dd750cfc80fff991ac8295134
onmax <onmax@users.noreply.github.com>
2023-03-07 22:08:04 +0000

    Add new icon: Sketchy arrow

---
+++ public/icons/icon_sketchy-arrow.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "sketchy arrow",
+  keywords: ["sketchy","arrow","illustration","drawing","doodle","sketch","pointing","direction","line","vector","symbol","graphic","pointer","visual","cartoon","curved","design","icon","arrowhead","arrows","scribble","simple","sketchiness","sketchy-style","swoosh","tail","tip"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 322 46"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M2 29.657c111.909-27.325 202.633-29.825 314.5 0"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-opacity=".3" stroke-width="3" d="M2 29.657c111.909-27.325 202.633-29.825 314.5 0"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="m307.008 1.992 12.711 28.976-29.003 12.704"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-opacity=".3" stroke-width="3" d="m307.008 1.992 12.711 28.976-29.003 12.704"/></svg>',
+  category: "Large",
+} satisfies Icon;
