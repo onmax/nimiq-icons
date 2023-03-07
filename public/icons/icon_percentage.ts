b3b9df51bec37585e2c914b4990712f45af29c35
onmax <onmax@users.noreply.github.com>
2023-03-07 22:03:50 +0000

    Add new icon: Percentage

---
+++ public/icons/icon_percentage.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "percentage",
+  keywords: ["percentage","proportion","ratio","share","fraction","percent","part","quantity","portion","measure","proportional","percentile","divide","percentagepoint","rate","proportionate","percentageincrease","percentagedecrease","percentagechange","percentagedifference","quota","calculate","partition","percentilerank","percentageshare","percentageoftotal"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 27 25"><path fill="currentColor" fill-rule="evenodd" d="M21.11 3.083a1.5 1.5 0 0 0-2.486-1.68L4.824 21.82a1.5 1.5 0 0 0 2.487 1.68l13.8-20.417ZM7.333 4.948A3.5 3.5 0 1 1 .461 6.28a3.5 3.5 0 0 1 6.872-1.332ZM23.23 23.16a3.5 3.5 0 1 0-1.332-6.872 3.5 3.5 0 0 0 1.332 6.872Z" clip-rule="evenodd"/></svg>',
+  category: "Small",
+} satisfies Icon;
