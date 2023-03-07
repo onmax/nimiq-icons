6d052d1726bf71e1292b49a1cea716546dac1bcb
onmax <onmax@users.noreply.github.com>
2023-03-07 22:24:31 +0000

    Add new icon: Headset

---
+++ public/icons/icon_headset.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "headset",
+  keywords: ["headset","headphones","earphones","earbuds","audio","listening","hearing","music","voice","sound","communication","telephone","calling","talking","gaming","gamingheadset","surroundsound","noisecancelling","wireless","bluetooth","headphonejack","mic","microphone","volumecontrol","comfort","style"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 52 52"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M42.853 49.472S49.603 37 49.562 25.695A23.782 23.782 0 0 0 2 25.867c.04 11.31 6.88 23.726 6.88 23.726"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M13.28 30.66 9.87 32.1a2.114 2.114 0 0 0-1.125 2.77l5.136 12.171a2.114 2.114 0 0 0 2.77 1.126l3.407-1.438a2.117 2.117 0 0 0 1.126-2.77L16.05 31.787a2.113 2.113 0 0 0-2.77-1.127Zm18.222 16.071 3.408 1.438a2.114 2.114 0 0 0 2.77-1.125l5.136-12.173a2.114 2.114 0 0 0-1.126-2.769l-3.408-1.438a2.113 2.113 0 0 0-2.77 1.126l-5.136 12.172a2.114 2.114 0 0 0 1.126 2.77Z"/></svg>',
+  category: "Small",
+} satisfies Icon;
