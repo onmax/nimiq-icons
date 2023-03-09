1c7cd6faca8649dc50bbb65c556876506f0f9f1b
onmax <onmax@users.noreply.github.com>
2023-03-09 02:19:03 +0000

    Add new icon: Facebook

---
+++ public/icons/icon_facebook.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "facebook",
+  keywords: ["facebook","socialmedia","networking","connect","share","like","comment","post","messenger","friends","groups","pages","events","timeline","profile","photos","videos","status","tagging","notifications","chatting","following","marketing","advertising","sharing","connecting"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 24"><path d="M13.8 7.7c-.1-.1-.2-.2-.3-.2H9.7V6.2c0-.3.1-.6.5-.6h2.9c.1 0 .3 0 .4-.2 0-.1.1-.2.1-.3V1.3c0-.3-.2-.5-.5-.5H8.9C4.2.8 3.8 4.7 3.8 5.9v1.6H1.4c-.3 0-.5.2-.5.5v3.8c0 .3.2.5.5.5h2.5v11c0 .3.2.5.5.5h4.9c.3 0 .5-.2.5-.5v-11h3.3c.3 0 .5-.2.5-.4l.4-3.8c0-.2-.1-.3-.2-.4z"/></svg>',
+  category: "Logo",
+} satisfies Icon;
