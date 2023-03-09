2592b291259ba2ecaf9b0be213346f0a6bb17ac8
onmax <onmax@users.noreply.github.com>
2023-03-09 15:53:58 +0000

    Add new icon: Bank large

---
+++ public/icons/icon_bank-large.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "bank large",
+  keywords: ["bank","savings","account","finance","money","deposit","withdrawal","loan","currency","banking","investment","credit","debit","capital","interest","wealth","savingsaccount","mortgage","budgeting","ATM","cash","bankingservices","cashflow","bankingsystem","financialinstitution","bankingproducts"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64"><path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" d="M1.5 8.2 32.1 2l30.4 6.2"/><path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" d="M8 7v5.7a1.7 1.7 0 0 0 1.7 1.7h45.7a1.6 1.6 0 0 0 1.6-1.7V7"/><path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" d="M14 16v30m7-30v30m7.5-30v30m7-31v31M43 15v31m7-31v31" opacity=".4"/><path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="3" d="M12 46.5h40c.5 0 1 .3 1.4.8.4.4.6 1.1.6 1.8v5.4H10v-5.4l.1-1 .5-.8.6-.6.8-.2Zm45.7 8H6.3C5 54.5 4 55.7 4 57.1v2.8c0 1.4 1 2.6 2.3 2.6h51.4c1.3 0 2.3-1.2 2.3-2.6v-2.8c0-1.4-1-2.6-2.3-2.6Z"/></svg>',
+  category: "Large",
+} satisfies Icon;
