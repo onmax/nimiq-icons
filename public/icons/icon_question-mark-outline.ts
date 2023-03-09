0b01baea7226f37cf870c921fc7825de42be85a2
onmax <onmax@users.noreply.github.com>
2023-03-09 15:49:51 +0000

    Add new icon: Question mark outline

---
+++ public/icons/icon_question-mark-outline.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "question mark outline",
+  keywords: ["question","inquiry","query","puzzle","doubt","conundrum","enigma","mystery","problem","quiz","quandary","dilemma","queries","quizzes","riddle","doubts","quest","quizzing","conundrums","mysteries","enigmas","puzzles","questions","problems","dilemmas","inquiries"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 23 39"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M1.812 11.467v-.051c0-5.374 4.289-9.714 9.662-9.714a9.647 9.647 0 0 1 9.662 9.662c-.052 5.322-4.34 9.662-9.714 9.662v5.166"/><path fill="currentColor" d="M11.5 38.118a2.059 2.059 0 1 0 0-4.118 2.059 2.059 0 0 0 0 4.118Z"/></svg>',
+  category: "Large",
+} satisfies Icon;
