f89008a148e02233cdda9ad08ae4c01e69b3858b
onmax <onmax@users.noreply.github.com>
2023-03-09 00:39:11 +0000

    Add new icon: Palette

---
+++ public/icons/icon_palette.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "palette",
+  keywords: ["palette","colors","colour","hues","shades","tints","tones","mix","blend","combination","rainbow","saturation","lightness","visual","art","design","paint","brush","artist","creation","imagination","expression","vibrant","muted","hue","shade","tint"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 54 51"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.615 45.385a26 26 0 1 1 36.77 0 12.437 12.437 0 0 1-17.587 0l-9.59-9.593a2.26 2.26 0 0 0-3.19 3.197 4.524 4.524 0 1 1-6.403 6.396Z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M38.982 18.207a3.392 3.392 0 1 1-4.8-4.793 3.392 3.392 0 0 1 4.8 4.793Zm3.843 11.05a2.261 2.261 0 1 0 0-4.522 2.261 2.261 0 0 0 0 4.522Zm-9.43 12.133a4.521 4.521 0 1 1 6.362-6.425 4.521 4.521 0 0 1-6.362 6.425ZM25.4 11.011a3.392 3.392 0 1 1-4.793 4.8 3.392 3.392 0 0 1 4.792-4.8h.001Z"/></svg>',
+  category: "Small",
+} satisfies Icon;
