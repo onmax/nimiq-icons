1233591a36f00a77ec4a696293d6ed798cb328c4
onmax <onmax@users.noreply.github.com>
2023-03-09 16:12:48 +0000

    Add new icon: nimiq.watch App

---
+++ public/icons/icon_nimiq.watch-app.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "nimiq.watch app",
+  keywords: ["nimiq","cryptocurrency","blockchain","digitalcurrency","payment","transaction","wallet","mining","security","decentralized","peer-to-peer","walletapp","nimiqwatch","cryptowallet","cryptoexchange","cryptotrading","cryptopayments","digitalwallet","securepayments","securetransactions","cryptomining","decentralizedcurrency","cryptosecurity","cryptoinvestments","cryptomarket","cryptotradingplatform"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 119 119"><path fill="currentColor" fill-rule="evenodd" d="M50.21 3.2A5.92 5.92 0 0 0 45.56.52H19.11a5.9 5.9 0 0 0-4.65 2.68L1.23 25.72a5.71 5.71 0 0 0 0 5.36l13.23 22.61a5.9 5.9 0 0 0 4.65 2.68h26.45a5.78 5.78 0 0 0 4.65-2.68L63.43 31a5.48 5.48 0 0 0 0-5.27L50.21 3.2Zm54.33 30.92a6.152 6.152 0 0 0-4.65-2.68H73.44a5.9 5.9 0 0 0-4.65 2.68l-13.22 22.7a5.66 5.66 0 0 0 0 5.36l13.22 22.61a5.92 5.92 0 0 0 4.65 2.68h26.45a5.897 5.897 0 0 0 4.65-2.68l13.23-22.61a5.524 5.524 0 0 0 0-5.27l-13.23-22.79Zm-54 31.1a5.581 5.581 0 0 0-4.46-2.68H19.46a5.891 5.891 0 0 0-4.64 2.68L1.68 87.92a5.53 5.53 0 0 0 0 5.27l13.23 22.61a6.147 6.147 0 0 0 4.64 2.68H46a6.006 6.006 0 0 0 4.64-2.68l13.23-22.7a5.71 5.71 0 0 0 0-5.36L50.54 65.22Z" clip-rule="evenodd"/></svg>',
+  category: "Logo",
+} satisfies Icon;
