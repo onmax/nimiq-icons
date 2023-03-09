6c26a437852338d502ed34aea426a75c99e705cc
onmax <onmax@users.noreply.github.com>
2023-03-09 00:25:49 +0000

    Add new icon: Dashboard

---
+++ public/icons/icon_dashboard.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "dashboard",
+  keywords: ["dashboard","controlpanel","monitor","analytics","metrics","data","insights","status","overview","gauge","graphics","visualization","information","summary","statistics","report","charts","graphs","scorecard","keyperformanceindicators","kpi","meters","gadgets","widgets","trends","analysis"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 64 64"><g clip-path="url(#a)"><path fill="currentColor" d="M1 18h19v44H1z" opacity=".4"/><rect width="61" height="61" x="1.5" y="1.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" rx="2"/><circle cx="10" cy="10" r="3" fill="currentColor" opacity=".4"/><circle cx="18" cy="10" r="3" fill="currentColor" opacity=".4"/><circle cx="26" cy="10" r="3" fill="currentColor" opacity=".4"/><path stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M2 18.5h60M20 19v44"/></g><defs><clipPath id="a"><path fill="currentColor" d="M0 64h64V0H0z"/></clipPath></defs></svg>',
+  category: "Large",
+} satisfies Icon;
