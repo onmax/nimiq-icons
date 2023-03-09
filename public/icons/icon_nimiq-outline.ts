c21af389350a578365e1c3270eaf83cf3edb662e
onmax <onmax@users.noreply.github.com>
2023-03-09 13:25:30 +0000

    Add new icon: Nimiq outline

---
+++ public/icons/icon_nimiq-outline.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "nimiq outline",
+  keywords: ["nimiq","blockchain","cryptocurrency","decentralized","distributed","network","payment","system","peer-to-peer","transaction","security","trustless","cryptography","mining","wallet","crypto","store","digital","currency","internet","money","exchange","value","crypto-currency","secure"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 42 38"><path stroke="currentColor" stroke-width="3" d="m39.352 17.408-7.78-13.814A3.106 3.106 0 0 0 28.875 2H13.318c-1.11 0-2.137.606-2.692 1.594l-7.78 13.814a3.26 3.26 0 0 0 0 3.188l7.78 13.814a3.09 3.09 0 0 0 2.692 1.594h15.559a3.09 3.09 0 0 0 2.691-1.594l7.78-13.814c.56-.988.559-2.2.004-3.188Z"/></svg>',
+  category: "Logo",
+} satisfies Icon;
