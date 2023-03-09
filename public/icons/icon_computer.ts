8ec5bd6d65a11657d2cd6c2f2bb672ac15a279ff
maximo <maximo@nimiq.com>
2023-03-08 20:36:14 -0600

    Fixed icons

--- public/icons/icon_computer.ts
+++ public/icons/icon_computer.ts
@@ -1,6 +1,6 @@
 export default {
     name: "computer",
     keywords: ["computer", "laptop", "device", "electronics", "screen", "keyboard", "mouse", "technology", "desktop", "PC", "work", "office", "computer icon", "laptop icon", "technology icon"],
-    svg: `<svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.25 18.333a14.263 14.263 0 0 0-6.25-1.25c-2.15-.06-4.288.367-6.25 1.25M9 14.417v2m4-2v2M19.75 1.5H2.25a.833.833 0 0 0-.834.833v10.834c0 .46.373.833.833.833h17.5c.46 0 .834-.373.834-.833V2.333a.834.834 0 0 0-.833-.833Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>`,
+    svg: `<svg viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.25 18.333a14.263 14.263 0 0 0-6.25-1.25c-2.15-.06-4.288.367-6.25 1.25M9 14.417v2m4-2v2M19.75 1.5H2.25a.833.833 0 0 0-.834.833v10.834c0 .46.373.833.833.833h17.5c.46 0 .834-.373.834-.833V2.333a.834.834 0 0 0-.833-.833Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>`,
 	"category": 'Small'
 } satisfies Icon;
