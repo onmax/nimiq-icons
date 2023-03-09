486131581d8d11af01a13b898548366ed948f2b2
onmax <onmax@users.noreply.github.com>
2023-03-09 02:21:36 +0000

    Add new icon: Blocks

---
+++ public/icons/icon_blocks.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "blocks",
+  keywords: ["block","building","structure","architecture","construction","stacking","buildingblocks","toy","stack","shape","design","game","lego","creativity","imagination","buildingmaterials","bricks","stackable","pattern","play","stackingblocks","interlocking","buildingset","jenga","tetris","stackinggame"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><rect width="22" height="8" x="1" y="1" stroke="currentColor" stroke-width="2" rx="2"/><rect width="12" height="9" x="24" y="12" fill="currentColor" rx="2" transform="rotate(90 24 12)"/><rect width="11" height="10" x="12" y="23" stroke="currentColor" stroke-width="2" rx="2" transform="rotate(-180 12 23)"/></svg>',
+  category: "Small",
+} satisfies Icon;
