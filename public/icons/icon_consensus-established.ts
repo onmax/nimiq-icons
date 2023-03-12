2d0557ba1abd5f4c488f3a046447cda44d58eaed
maximo <maximo@nimiq.com>
2023-03-12 16:35:01 +0000

    updated icons

--- public/icons/icon_consensus-established.ts
+++ public/icons/icon_consensus-established.ts
@@ -2,5 +2,5 @@ export default {
   name: "consensus established",
   keywords: ["consensus","agreement","settlement","accord","unity","harmony","alliance","concurrence","concord","amity","compromise","arrangement","understanding","resolution","synchrony","coalition","cooperation","pact","unityofpurpose","commonground","solidarity","meetingofminds","mutualunderstanding","consent","conformity","accordance"],
   svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 44 41"><g stroke="currentColor" stroke-linecap="round" stroke-width="2" opacity=".4"><path d="M19.388 38.998C9.206 38.84 1 30.538 1 20.318c0-10.317 8.364-18.68 18.681-18.68 8.425 0 15.547 5.576 17.876 13.239"/><path d="M27.097 22.596a46.89 46.89 0 0 0 .055-2.277c0-10.317-3.346-18.681-7.473-18.681-4.126 0-7.472 8.364-7.472 18.68 0 10.073 3.188 18.283 7.18 18.668M2 15.434h35.551M2.875 27.838h17.434"/></g><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="m27.152 34.73 4.27 4.27 10.675-14.945"/></svg>',
-  category: "Small",
+  category: "Large",
 } satisfies Icon;
--- public/icons/icon_tumbler.ts
+++ public/icons/icon_tumbler.ts
@@ -2,5 +2,5 @@ export default {
   name: "tumbler",
   keywords: ["tumbler","drinkingcup","travelmug","drinkware","insulatedcup","reusablecup","thermos","drinkcontainer","stainlesssteelcup","vacuumflask","drinkingvessel","thermalmug","hotbeverage","coldbeverage","drinkingvessel","coffeemug","teacup","traveltumbler","insulatedtumbler","plastictumbler","ceramictumbler","stainlesssteeltumbler","thermaltumbler","doublewalltumbler","plasticinsulatedcup","travelcup"],
   svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 35 46"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.39 22.043h27.74M4.107 33.522h26.345m-17.974-5.739h9.565M31.609 6.74H2.914l1.695-5.086A.953.953 0 0 1 5.516 1h23.492a.96.96 0 0 1 .907.654l1.694 5.086Z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M29.752 43.925a.96.96 0 0 1-.957.897H5.726a.956.956 0 0 1-.957-.897L2.914 14.213h28.695l-1.857 29.712ZM32.565 6.56H1.956A.956.956 0 0 0 1 7.517v5.74c0 .528.428.956.956.956h30.609a.957.957 0 0 0 .957-.957v-5.74a.957.957 0 0 0-.957-.955Z"/></svg>',
-  category: "Small",
+  category: "Large",
 } satisfies Icon;
