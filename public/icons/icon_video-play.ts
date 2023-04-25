715db659c5c149bba83b631b489a42c6dfd91dda
Max <maximo@nimiq.com>
2023-04-25 20:28:52 +0200

    Update icon_video-play.ts

--- public/icons/icon_video-play.ts
+++ public/icons/icon_video-play.ts
@@ -1,6 +1,6 @@
 export default {
   name: "video play",
   keywords: ["video","play","movie","streaming","watch","online","videoclip","video-on-demand","broadcast","television","television-show","television-program","entertainment","media","recording","cinema","clip","youtube","vimeo","viewing","movie-theater","film","player","stream","television-channel","cable-channel"],
-  svg: '<svg width="31" height="38" viewBox="0 0 31 38" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.068.312C1.737-.528 0 .428 0 2.003v33.164c0 1.575 1.737 2.532 3.068 1.69l26.255-16.58a2 2 0 0 0 0-3.383L3.068.312Z" fill="#fff"/></svg>',
+  svg: '<svg viewBox="0 0 31 38" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.068.312C1.737-.528 0 .428 0 2.003v33.164c0 1.575 1.737 2.532 3.068 1.69l26.255-16.58a2 2 0 0 0 0-3.383L3.068.312Z" fill="currentColor"/></svg>',
   category: "Small",
 } satisfies Icon;
