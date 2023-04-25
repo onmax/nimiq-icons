64e3e03fc8b4b87088b8e492334e9547373a3b3b
onmax <onmax@users.noreply.github.com>
2023-04-25 18:25:13 +0000

    Add new icon: Video Play

---
+++ public/icons/icon_video-play.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "video play",
+  keywords: ["video","play","movie","streaming","watch","online","videoclip","video-on-demand","broadcast","television","television-show","television-program","entertainment","media","recording","cinema","clip","youtube","vimeo","viewing","movie-theater","film","player","stream","television-channel","cable-channel"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="147" height="154" fill="none"><path fill="currentColor" d="M61.492 31.437c-1.331-.841-3.068.116-3.068 1.691v33.164c0 1.575 1.737 2.532 3.068 1.69l26.255-16.581a2 2 0 0 0 0-3.382L61.492 31.437Z"/></svg>',
+  category: "Small",
+} satisfies Icon;
