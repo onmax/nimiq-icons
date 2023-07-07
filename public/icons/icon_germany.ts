f4bff0371a94c1eb31c591b8ec4df3d78352a5b2
onmax <onmax@users.noreply.github.com>
2023-07-07 05:17:44 +0000

    Add new icon: Germany

---
+++ public/icons/icon_germany.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "germany",
+  keywords: ["germany","german","deutschland","europe","eu","europeanunion","bundesrepublikdeutschland","germanculture","germanlanguage","germanhistory","berlin","munich","frankfurt","hamburg","cologne","stuttgart","dresden","leipzig","bavaria","brandenburg","saxony","saxony-anhalt","saarland","thuringia","rhineland-palatinate","mecklenburg-vorpommern"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="21" height="16" fill="none"><mask id="a" width="21" height="16" x="0" y="0" maskUnits="userSpaceOnUse" style="mask-type:luminance"><path fill="currentColor" d="M.5.234h20v15H.5z"/></mask><g fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" mask="url(#a)"><path d="M.5 10.234h20v5H.5v-5ZM.5 5.234h20v5H.5v-5ZM.5.234h20v5H.5v-5Z"/></g></svg>',
+  category: "Country",
+} satisfies Icon;
