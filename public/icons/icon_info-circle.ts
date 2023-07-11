929f6fab55d885c8d1b12b2e608bfa09d33ae38e
onmax <onmax@users.noreply.github.com>
2023-07-11 07:19:40 +0000

    Add new icon: Info circle

---
+++ public/icons/icon_info-circle.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "info circle",
+  keywords: ["information","help","guidance","advice","instruction","clarification","explanation","education","knowledge","tutorial","directions","tips","hints","pointers","data","understanding","awareness","tutoring","clue","illustration","manual","thesis","definition","elucidation","tractate","commentary"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="nq-icon"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M8 7.833v3.5"/><circle cx="8" cy="4.75" r=".5" fill="currentColor" stroke="currentColor"/><circle cx="8" cy="8" r="7.25" fill="none" stroke="currentColor" stroke-width="1.5"/></svg>',
+  category: "Small",
+} satisfies Icon;
