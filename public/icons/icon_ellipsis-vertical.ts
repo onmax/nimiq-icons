484016b561ea84dbca9252d8492dcb66da698ab3
Max <maximo@nimiq.com>
2023-07-19 15:47:16 +0700

    Update icon_ellipsis-vertical.ts

--- public/icons/icon_ellipsis-vertical.ts
+++ public/icons/icon_ellipsis-vertical.ts
@@ -1,6 +1,6 @@
 export default {
   name: "ellipsis vertical",
   keywords: ["ellipsis","vertical","three-dots","pause","break","pause-mark","separator","cut-off","interruption","omission","elliptical","abbreviation","interrupted","incomplete","dividing","gapping","halting","abridged","suspension","condensation","truncation","shortening","stopping","discontinuation","hesitation","break-off"],
-  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><g fill="currentColor" opacity=".5"><circle cx="10" cy="2" r="2"/><circle cx="10" cy="10" r="2"/><circle cx="10" cy="18" r="2"/></g></svg>',
+  svg: ' <svg viewBox="0 0 4 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 4a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM2 12a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM2 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" fill="currentColor" />  </svg>',
   category: "Small",
 } satisfies Icon;
