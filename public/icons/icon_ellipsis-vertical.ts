b195c43093d4d02683eab06ff6c2ae027d8b06cc
onmax <onmax@users.noreply.github.com>
2023-07-19 08:35:47 +0000

    Add new icon: Ellipsis vertical

---
+++ public/icons/icon_ellipsis-vertical.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "ellipsis vertical",
+  keywords: ["ellipsis","vertical","three-dots","pause","break","pause-mark","separator","cut-off","interruption","omission","elliptical","abbreviation","interrupted","incomplete","dividing","gapping","halting","abridged","suspension","condensation","truncation","shortening","stopping","discontinuation","hesitation","break-off"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><g fill="currentColor" opacity=".5"><circle cx="10" cy="2" r="2"/><circle cx="10" cy="10" r="2"/><circle cx="10" cy="18" r="2"/></g></svg>',
+  category: "Small",
+} satisfies Icon;
