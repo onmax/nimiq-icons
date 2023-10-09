f5d36a89f56499bf5531eeafacf681f7c875efda
onmax <onmax@users.noreply.github.com>
2023-10-09 11:39:14 +0000

    Add new icon: Person 4

---
+++ public/icons/icon_person-4.ts
@@ -0,0 +1,6 @@
+export default {
+  name: "person 4",
+  keywords: ["person","human","man","woman","individual","being","face","character","identity","figure","portrait","avatar","citizen","personality","visage","expression","mankind","self","soul","emotion","body","appearance","persona","image","mugshot","physiognomy"],
+  svg: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 35 42"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.3 41c3.477 0 11.25-4.303 12.367-10.913a2.5 2.5 0 0 1 1.435-1.864c2.703-1.226 3.38-6.13 1.198-7.223 1.25-7.465 1.25-20-15-20S1.05 13.535 2.3 21C.113 22.093.8 27 3.496 28.223a2.5 2.5 0 0 1 1.435 1.864C6.039 36.697 13.819 41 17.299 41Z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1.922 18.017a21.37 21.37 0 0 0 8.968-7.195 21.172 21.172 0 0 0 21.885 5.342M13.6 32.252a5.438 5.438 0 0 0 7.5 0M9.967 27.583a4.375 4.375 0 1 0 0-8.75 4.375 4.375 0 0 0 0 8.75Zm13.75 0a4.375 4.375 0 1 0 0-8.75 4.375 4.375 0 0 0 0 8.75Zm-9.375-4.375h5"/></svg>',
+  category: "Small",
+} satisfies Icon;
